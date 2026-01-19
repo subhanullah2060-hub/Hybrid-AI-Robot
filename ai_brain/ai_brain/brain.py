f650c7f13bd80dceea47b84f2d6d48b79220b5f4
subhanullah2060-hub <subhanullah2060@gmail.com>
2026-01-19 21:46:57 +0500

    Update brain.py

--- ai_brain/ai_brain/brain.py
+++ ai_brain/ai_brain/brain.py
@@ -1,1 +1,16 @@
 
+def think(user_input):
+    if "hello" in user_input.lower():
+        return "Hello human 👋"
+
+    elif "your name" in user_input.lower():
+        return "I am a Hybrid AI Robot"
+
+    elif "move" in user_input.lower():
+        return "COMMAND:MOVE"
+
+    elif "stop" in user_input.lower():
+        return "COMMAND:STOP"
+
+    else:
+        return "I am learning..."
